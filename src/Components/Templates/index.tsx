66204c2f4ed1941e39fb5ea46ff97363a95c6478
mohith k <mohith.k@kaaratech.com>
2021-03-16 17:18:41 +0530

    Folder structure and implemented components

---
+++ src/Components/Templates/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Organisms/header';
+
+
+
+const Template: React.FC = () => {
+    return (
+        <div className="template">
+            <Header />
+        </div>
+    )
+}
+export default Template
